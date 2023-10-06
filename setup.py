591e6b1131049b7158aec0c47b946c5b3bdb26f8
Alexander <jofalk@student.ethz.ch>
2023-10-07 00:17:28 +0200

    Removed redundant outer folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This projects investigates which factors are useful in predicting S&P 500 returns',
+    author='"Alexander, Jannic, Julius"',
+    license='MIT',
+)
