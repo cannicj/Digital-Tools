0a10b9126d701154e9029df4f7a096e40024b7c2
janni <jannicnoel.cavegn@uzh.ch>
2023-11-06 18:58:10 +0100

    update the get_save_data and process_data script to include spx data

--- src/data/get_save_data.py
+++ src/data/get_save_data.py
@@ -1,26 +1,43 @@
 from download_rawdata_functions import get_currency_pair_data
 import pandas as pd
 import os
+from pandas_datareader import DataReader
+import datetime
+import time
 
+#download all the currency data
+print("Downloading data for all needed currencies.")
 currency_pairs = ["USDEUR","USDJPY","USDGBP","USDCHF","USDAUD","USDCAD","USDNZD","USDSEK","USDSGD","USDNOK"]
 # Create an empty DataFrame to store the combined data
 combined_currency_data = pd.DataFrame()
-
 # Fetch data for each currency pair and combine into a single DataFrame
 for pair in currency_pairs:
     currency_data = get_currency_pair_data(pair)
     if currency_data is not None:
         combined_currency_data[pair] = currency_data[currency_data.columns[0]]
+    time.sleep(1)
+
+#download all the currency data
+print("Downloading SP500 data.")
+ticker = 'SP500'
+# download data
+start_date = datetime.datetime(2000, 1, 1)
+spx_data = DataReader(ticker, 'fred', start_date)
 
 # Define the directory to save the CSV file
 output_directory = '../../data/raw'  # Relative path from the current directory
-
 # Create the directory if it doesn't exist
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
+# save the currency file as .csv
 # Define the output file path
-output_file = os.path.join(output_directory, 'combined_currency_data.csv')
-
+output_file = os.path.join(output_directory, 'currency_data.csv')
 # Save the combined data to a CSV file in the specified directory
 combined_currency_data.to_csv(output_file, index=True, mode = 'w')
+
+# save the spx datafile as .csv
+# Define the output file path
+output_file = os.path.join(output_directory, 'spx_data.csv')
+# Save the combined data to a CSV file in the specified directory
+spx_data.to_csv(output_file, index=True, mode = 'w')
--- src/data/process_data.py
+++ src/data/process_data.py
@@ -5,26 +5,31 @@ import os
 import pandas as pd
 import numpy as np
 
-def compute_log_returns(input_file, output_directory):
+def compute_log_returns(input_file):
     # Load the previously saved combined currency data
-    combined_currency_data = pd.read_csv(input_file, index_col=0)
-    combined_currency_data = combined_currency_data.dropna()
+    data = pd.read_csv(f'../../data/raw/{input_file}', index_col=0)
+    data = data.dropna()
     # Compute log returns for each currency column
     log_returns = pd.DataFrame()
-    for column in combined_currency_data.columns:
-        log_returns[column] = np.log(combined_currency_data[column]) - np.log(combined_currency_data[column].shift(1))
+    for column in data.columns:
+        log_returns[column] = np.log(data[column]) - np.log(data[column].shift(1))
     log_returns = log_returns.round(5)
     # Define the output file path for processed data
-    output_file = os.path.join(output_directory, 'log_returns_currency_data.csv')
+    output_file = os.path.join('../../data/processed', f'log_returns_{input_file}')
 
     # Save the log returns to a CSV file in the specified directory
     log_returns.to_csv(output_file, index=True, mode = 'w')
 
-# Define the input file path
-input_file_path = '../../data/raw/combined_currency_data.csv'
-
-# Define the output directory
-output_directory = '../../data/processed'
-
+print("Processing currency data.")
+# Define the input file name
+currency_data = 'currency_data.csv'
 # Compute log returns and save the processed data
-compute_log_returns(input_file_path, output_directory)
+compute_log_returns(currency_data)
+
+print("Processing SPX data.")
+# Define the input file name
+spx_data = 'spx_data.csv'
+# Compute log returns and save the processed data
+compute_log_returns(spx_data)
+
+
