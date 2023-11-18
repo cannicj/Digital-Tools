351fc5c034fdf5804c0e8c8b749bb6080ca73792
juliusraschke <113939532+juliusraschke@users.noreply.github.com>
2023-11-18 15:24:00 +0000

    Update decision_tree_classifier.py

--- models/decision_tree_classifier.py
+++ models/decision_tree_classifier.py
@@ -14,18 +14,22 @@ import matplotlib.dates as mdates
 
 def decision_tree_classifier(dataframe, currencies=None, include_sp500=True,lag=1,train_size=0.75,max_depth=10):
      """
-    Applies a Decision Tree Classifier to financial data for binary outcome prediction.
+    Trains a Decision Tree Classifier on financial data to predict binary outcomes and evaluates its performance.
+
+    This function processes financial data, which can include currency pairs and optionally S&P 500 data, and applies a Decision Tree Classifier for binary classification. It calculates the model's accuracy and plots cumulative returns for comparison.
 
     Parameters:
-    - DataFrame: The main dataset including currency and optionally S&P 500 data.
-    - currencies (list of str, optional): List of currency pair columns from the dataset. Includes all if None.
-    - train_size (float): Proportion of the dataset used for training the model. Default is 0.75.
-    - include_sp500 (bool): Flag to decide if S&P 500 data should be included. Default is False.
-    - lag (int): Number of periods to lag the S&P 500 data. Default is 1.
-    - max_depth (int): Maximum depth of the decision tree. Default is 10.
+    - dataframe (pd.DataFrame): The dataset containing the financial data. It should have a 'DATE' column, currency data columns, and optionally an 'SP500' column.
+    - currencies (list of str, optional): List of currency columns to include in the analysis. If None, all available currencies in the dataframe will be used.
+    - include_sp500 (bool): Determines whether to include the S&P 500 data in the analysis. Defaults to True.
+    - lag (int): The number of periods by which to lag the response variable for prediction. Defaults to 1.
+    - train_size (float): The proportion of the dataset to use for training the model. The rest will be used for testing. Defaults to 0.75.
+    - max_depth (int): The maximum depth of the decision tree. Helps to control the complexity of the model. Defaults to 10.
 
     Returns:
-    - tuple: Contains DataFrame of model accuracies and DataFrame or plot of model's financial performance.
+    - tuple: A tuple containing a DataFrame of model accuracies and another DataFrame with the cumulative returns for both the 'Long' strategy and the Decision Tree Classifier model. It also generates a plot comparing these returns.
+
+    The function prints the accuracy of the model and displays a plot of the cumulative returns. The accuracies DataFrame includes 'in sample' and 'out of sample' accuracy of the Decision Tree Classifier.
     """
     
     # Selecting columns based on currencies if provided
