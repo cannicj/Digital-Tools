6a53ae45e0874f4103631f0b1675345c48d203ac
AlexanderFalkETH <jofalk@student.ethz.ch>
2023-11-19 22:49:29 +0100

    Added colorblind palette to plotting function

--- src/visualization/plot_results.py
+++ src/visualization/plot_results.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import seaborn as sns
 def plot_results(dataframe, include_sp500):
     # Plotting
     plt.figure(figsize=(14, 6))
     cols = dataframe.shape[1]
-    # Define a list of colors or a colormap
-    colors = ['red', 'green', 'blue', 'orange', 'purple']
+    # Set the Seaborn color palette
+    sns.set_palette("colorblind")
     for i in range(1, cols):
-     plt.plot(dataframe["DATE"], dataframe.iloc[:,i], colors[i-1], label= dataframe.columns[i])
+     plt.plot(dataframe["DATE"], dataframe.iloc[:,i], label= dataframe.columns[i])
 
     plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
     plt.legend()
     plt.title("Financial Performance Prediction" + (" with " if include_sp500 else " without ") + "S&P500")
     plt.xlabel('Date')
-    plt.ylabel('CumSum Return')
+    plt.ylabel('Cumulative Return')
     plt.savefig('classifier.svg', dpi=1000)
     plt.show()
