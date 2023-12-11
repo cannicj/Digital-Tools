85f817fd78ba18ee2716a6f51502912a3afba81d
juliusraschke <113939532+juliusraschke@users.noreply.github.com>
2023-12-11 22:33:01 +0000

    Update run_simulations.py

--- src/visualization/run_simulations.py
+++ src/visualization/run_simulations.py
@@ -22,7 +22,9 @@ def run_simulations(dataframe, currencies, include_sp500, lag, train_size, long_
         _, svm_results = support_vector_machine(dataframe, currencies, include_sp500, lag, train_size, seed, long_only)
         if i == 0:
             svm_dates = svm_results['DATE']
+            svm_SP500 = svm_results['SP500']
             svm_results_list = pd.concat([svm_results_list, svm_dates], axis=1)
+            svm_results_list = pd.concat([svm_results_list, svm_SP500], axis=1)
         svm_relevant_column = svm_results.iloc[:, -1].rename(f'SVM Simulation {i+1}')
         svm_results_list = pd.concat([svm_results_list, svm_relevant_column], axis=1)
 
@@ -30,7 +32,9 @@ def run_simulations(dataframe, currencies, include_sp500, lag, train_size, long_
         _, rf_results = randomforest_classifier(dataframe, currencies, include_sp500, lag, train_size, seed, long_only)
         if i == 0:
             rf_dates = rf_results['DATE']
+            rf_SP500 = rf_results['SP500']
             rf_results_list = pd.concat([rf_results_list, rf_dates], axis=1)
+            rf_results_list = pd.concat([rf_results_list, rf_SP500], axis=1)
         rf_relevant_column = rf_results.iloc[:, -1].rename(f'RF Simulation {i+1}')
         rf_results_list = pd.concat([rf_results_list, rf_relevant_column], axis=1)
 
@@ -38,7 +42,9 @@ def run_simulations(dataframe, currencies, include_sp500, lag, train_size, long_
         _, dt_results = decision_tree_classifier(dataframe, currencies, include_sp500, lag, train_size, seed, long_only, max_depth=100)
         if i == 0:
             dt_dates = dt_results['DATE']
+            dt_SP500 = dt_results['SP500']
             dt_results_list = pd.concat([dt_results_list, dt_dates], axis=1)
+            dt_results_list = pd.concat([dt_results_list, dt_SP500], axis=1)
         dt_relevant_column = dt_results.iloc[:, -1].rename(f'DT Simulation {i+1}')
         dt_results_list = pd.concat([dt_results_list, dt_relevant_column], axis=1)
     
@@ -47,4 +53,3 @@ def run_simulations(dataframe, currencies, include_sp500, lag, train_size, long_
     dt_results_list['DATE'] = pd.to_datetime(dt_results_list['DATE'], format='%Y-%m-%d')
     
     return svm_results_list, rf_results_list, dt_results_list
-
